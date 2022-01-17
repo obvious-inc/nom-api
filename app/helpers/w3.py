1470050aa03bfd91af7956b4a56c3d22a4603fbc
prego <pedropregueiro@gmail.com>
2022-01-17 11:37:16 +0100

    add local checksum address

--- app/helpers/w3.py
+++ app/helpers/w3.py
@@ -2,10 +2,15 @@ from functools import cache
 
 from ens import ENS
 from eth_account.messages import encode_defunct
+from eth_typing import ChecksumAddress
 from eth_utils import ValidationError
 from web3 import Web3
 
 from app.config import get_settings
+
+
+def checksum_address(address: str) -> ChecksumAddress:
+    return Web3().toChecksumAddress(address)
 
 
 def get_wallet_address_from_signed_message(message: str, signature: str) -> str:
@@ -21,10 +26,6 @@ def get_wallet_address_from_signed_message(message: str, signature: str) -> str:
 async def get_ens_primary_name_for_address(wallet_address: str) -> str:
     settings = get_settings()
     web3_client = Web3(Web3.WebsocketProvider(settings.web3_provider_url_ws))
-    checksum_address = web3_client.toChecksumAddress(wallet_address)
-    ens_name = ENS.fromWeb3(web3_client).name(checksum_address)
+    wallet_address = checksum_address(wallet_address)
+    ens_name = ENS.fromWeb3(web3_client).name(wallet_address)
     return ens_name
-
-
-def checksum_address(address: str) -> str:
-    return Web3().toChecksumAddress(address)
