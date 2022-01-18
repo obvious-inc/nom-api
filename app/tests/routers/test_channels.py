ac5bcf48750b930c8ae9fdbec01e6f8f1553977b
prego <pedropregueiro@gmail.com>
2022-01-18 11:20:26 +0100

    add emoji channel test

--- app/tests/routers/test_channels.py
+++ app/tests/routers/test_channels.py
@@ -94,3 +94,26 @@ class TestChannelsRoutes:
         assert json_response["name"] == data["name"]
         assert "server" in json_response
         assert json_response["server"] == str(server.id)
+
+    @pytest.mark.asyncio
+    async def test_create_server_channel_with_emojis(
+        self, app: FastAPI, db: Database, current_user: User, server: Server, authorized_client: AsyncClient
+    ):
+        data = {
+            "kind": "server",
+            "name": "📣-fancy-announcements",
+            "server": str(server.id),
+        }
+
+        response = await authorized_client.post("/channels", json=data)
+        assert response.status_code == 201
+        json_response = response.json()
+        assert json_response != {}
+        assert "kind" in json_response
+        assert json_response["kind"] == data["kind"]
+        assert "owner" in json_response
+        assert json_response["owner"] == str(current_user.id)
+        assert "name" in json_response
+        assert json_response["name"] == data["name"]
+        assert "server" in json_response
+        assert json_response["server"] == str(server.id)
