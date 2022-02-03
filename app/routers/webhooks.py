8d93ddf4bc321ef390b0e02ad6e367b7db296364
prego <pedropregueiro@gmail.com>
2022-02-03 22:31:58 +0100

    pretty code = better code

--- app/routers/webhooks.py
+++ app/routers/webhooks.py
@@ -1,59 +1,17 @@
 import asyncio
-import json
 import logging
 from typing import Optional
 
-from bson import ObjectId
 from fastapi import APIRouter, Depends, Header, HTTPException, Request
 from starlette import status
 
 from app.helpers.connection import get_db
 from app.helpers.websockets import pusher_client
-from app.models.user import User
-from app.schemas.ws_events import CreateMarkChannelReadEvent
-from app.services.users import get_user_by_id
-from app.services.webhooks import process_channel_mark_event
-from app.services.websockets import broadcast_connection_ready
+from app.services.webhooks import handle_pusher_event
 
 logger = logging.getLogger(__name__)
 
 router = APIRouter()
-
-
-async def process_webhook_events(events: list[dict]):
-    for event in events:
-        try:
-            channel_name = event["channel"]
-            user_id = channel_name.split("-")[1]
-            if event["name"] == "channel_occupied":
-                update_data = {"$addToSet": {"online_channels": channel_name}}
-            elif event["name"] == "channel_vacated":
-                update_data = {"$pull": {"online_channels": channel_name}}
-            elif event["name"] == "client_event":
-                user = await get_user_by_id(user_id)
-                if not user:
-                    raise Exception(f"Missing user. [user_id={user_id}]")
-
-                client_event: str = event["event"]
-                if client_event == "client-connection-request":
-                    await broadcast_connection_ready(current_user=user, channel=channel_name)
-                elif client_event.startswith("client-channel-mark"):
-                    event_data = json.loads(event["data"])
-                    single_channel_id = event_data.pop("channel_id")
-                    if single_channel_id:
-                        event_data["channel_ids"] = [single_channel_id]
-                    event_model = CreateMarkChannelReadEvent(**event_data)
-                    await process_channel_mark_event(event_model=event_model, current_user=user)
-                logger.info(
-                    "client event handled successfully. [client_event=%s, channel=%s]", client_event, channel_name
-                )
-                return
-            else:
-                raise NotImplementedError(f"not expected event: {event}")
-
-            await User.collection.update_one(filter={"_id": ObjectId(user_id)}, update=update_data)
-        except Exception:
-            logger.exception("Problems handling webhook event. [event=%(name)s, channel=%(channel)s]", event)
 
 
 @router.post("/pusher", include_in_schema=False)
@@ -77,6 +35,6 @@ async def post_pusher_webhooks(
             detail="Could not validate webhook",
         )
 
-    asyncio.create_task(process_webhook_events(events=webhook["events"]))
+    [asyncio.create_task(handle_pusher_event(event=event)) for event in webhook["events"]]
 
     return {"received": "ok"}
--- app/services/webhooks.py
+++ app/services/webhooks.py
@@ -1,9 +1,73 @@
+import json
+import logging
+
 from app.models.user import User
 from app.schemas.ws_events import CreateMarkChannelReadEvent
 from app.services.channels import update_channels_read_state
+from app.services.users import get_user_by_id
+from app.services.websockets import broadcast_connection_ready
+
+logger = logging.getLogger(__name__)
 
 
-async def process_channel_mark_event(event_model: CreateMarkChannelReadEvent, current_user: User):
+async def _get_user_from_event(event: dict) -> User:
+    channel_name = event["channel"]
+    user_id = channel_name.split("-")[1]
+    user = await get_user_by_id(user_id)
+    if not user:
+        raise Exception(f"Missing user. [user_id={user_id}]")
+    return user
+
+
+async def handle_pusher_event(event: dict):
+    event_name = event["name"]
+    if event_name == "client_event":
+        return await handle_pusher_client_event(event)
+
+    channel_name = event["channel"]
+    user = await _get_user_from_event(event)
+
+    if event_name == "channel_occupied":
+        await process_channel_occupied_event(channel_name=channel_name, current_user=user)
+    elif event_name == "channel_vacated":
+        await process_channel_vacated_event(channel_name=channel_name, current_user=user)
+    else:
+        raise NotImplementedError(f"not expected event: {event}")
+
+    logger.info("pusher event handled successfully. [event=%s, channel=%s]", event_name, channel_name)
+
+
+async def handle_pusher_client_event(event: dict):
+    channel_name = event["channel"]
+    user = await _get_user_from_event(event)
+
+    client_event: str = event["event"]
+    if client_event == "client-connection-request":
+        await broadcast_connection_ready(current_user=user, channel=channel_name)
+    elif client_event.startswith("client-channel-mark"):
+        event_data = json.loads(event["data"])
+        single_channel_id = event_data.pop("channel_id")
+        if single_channel_id:
+            event_data["channel_ids"] = [single_channel_id]
+        event_model = CreateMarkChannelReadEvent(**event_data)
+        await process_channel_mark_read_event(event_model=event_model, current_user=user)
+    else:
+        raise NotImplementedError(f"not expected client event: {event}")
+
+    logger.info("client event handled successfully. [client_event=%s, channel=%s]", client_event, channel_name)
+
+
+async def process_channel_occupied_event(channel_name: str, current_user: User):
+    update_data = {"$addToSet": {"online_channels": channel_name}}
+    await User.collection.update_one(filter={"_id": current_user.pk}, update=update_data)
+
+
+async def process_channel_vacated_event(channel_name: str, current_user: User):
+    update_data = {"$pull": {"online_channels": channel_name}}
+    await User.collection.update_one(filter={"_id": current_user.pk}, update=update_data)
+
+
+async def process_channel_mark_read_event(event_model: CreateMarkChannelReadEvent, current_user: User):
     await update_channels_read_state(
         channel_ids=event_model.channel_ids, last_read_at=event_model.last_read_at, current_user=current_user
     )
