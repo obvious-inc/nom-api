893736fc03b0576e29469aac3a4b3c08bc2237dc
prego <pedropregueiro@gmail.com>
2022-02-23 12:14:57 +0100

    remove unnecessary logic from sockets

--- app/services/websockets.py
+++ app/services/websockets.py
@@ -4,13 +4,12 @@ from typing import List, Optional, Union
 from app.helpers.websockets import pusher_client
 from app.helpers.ws_events import WebSocketServerEvent
 from app.models.base import APIDocument
-from app.models.channel import Channel, ChannelReadState
+from app.models.channel import Channel
 from app.models.message import Message
 from app.models.server import Server, ServerMember
 from app.models.user import User
-from app.services.channels import get_server_channels
+from app.services.base import get_connection_ready_data
 from app.services.crud import get_item_by_id, get_items
-from app.services.servers import get_server_members, get_user_servers
 from app.services.users import get_user_by_id
 
 logger = logging.getLogger(__name__)
@@ -74,46 +73,7 @@ async def broadcast_message_event(
 
 
 async def broadcast_connection_ready(current_user: User, channel: str):
-    data = {"user": current_user.dump(), "servers": []}
-
-    servers = await get_user_servers(current_user=current_user)
-    for server in servers:
-        server_data = {"id": str(server.id), "name": server.name, "owner": str(server.owner.pk)}
-        channels = await get_server_channels(server_id=str(server.id), current_user=current_user)
-        members = await get_server_members(server_id=str(server.id), current_user=current_user)
-
-        server_data.update(
-            {
-                "channels": [
-                    {
-                        "id": str(channel.id),
-                        "last_message_at": channel.last_message_at.isoformat() if channel.last_message_at else None,
-                        "name": channel.name,
-                    }
-                    for channel in channels
-                ],
-                "members": [
-                    {
-                        "id": str(member.id),
-                        "user": str(member.user.pk),
-                        "server": str(member.server.pk),
-                        "display_name": member.display_name,
-                    }
-                    for member in members
-                ],
-            }
-        )
-
-        data["servers"].append(server_data)
-
-    read_states = await get_items(
-        filters={"user": current_user}, result_obj=ChannelReadState, current_user=current_user, size=None
-    )
-    data["read_states"] = [
-        {"channel": str(read_state.channel.pk), "last_read_at": read_state.last_read_at.isoformat()}
-        for read_state in read_states
-    ]
-
+    data = await get_connection_ready_data(current_user=current_user)
     await pusher_broadcast_messages(
         event=WebSocketServerEvent.CONNECTION_READY, data=data, current_user=current_user, pusher_channel=channel
     )
