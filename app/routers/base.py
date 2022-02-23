cbc35ab25eb19380b72a0d6898ee83f1edb82912
prego <pedropregueiro@gmail.com>
2022-02-23 09:26:07 +0100

    add endpoint to get initial user/channel data

--- app/routers/base.py
+++ app/routers/base.py
@@ -1,4 +1,8 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+
+from app.dependencies import get_current_user
+from app.models.user import User
+from app.services.base import get_connection_ready_data
 
 router = APIRouter()
 
@@ -6,3 +10,8 @@ router = APIRouter()
 @router.get("/", include_in_schema=False)
 def get_index():
     return {"status": "ok"}
+
+
+@router.get("/ready", include_in_schema=False)
+async def get_connection_ready(current_user: User = Depends(get_current_user)):
+    return await get_connection_ready_data(current_user=current_user)
---
+++ app/services/base.py
@@ -0,0 +1,48 @@
+from app.models.channel import ChannelReadState
+from app.models.user import User
+from app.services.channels import get_server_channels
+from app.services.crud import get_items
+from app.services.servers import get_server_members, get_user_servers
+
+
+async def get_connection_ready_data(current_user: User) -> dict:
+    data = {"user": current_user.dump(), "servers": []}
+    servers = await get_user_servers(current_user=current_user)
+    for server in servers:
+        server_data = {"id": str(server.id), "name": server.name, "owner": str(server.owner.pk)}
+        channels = await get_server_channels(server_id=str(server.id), current_user=current_user)
+        members = await get_server_members(server_id=str(server.id), current_user=current_user)
+
+        server_data.update(
+            {
+                "channels": [
+                    {
+                        "id": str(channel.id),
+                        "last_message_at": channel.last_message_at.isoformat() if channel.last_message_at else None,
+                        "name": channel.name,
+                    }
+                    for channel in channels
+                ],
+                "members": [
+                    {
+                        "id": str(member.id),
+                        "user": str(member.user.pk),
+                        "server": str(member.server.pk),
+                        "display_name": member.display_name,
+                    }
+                    for member in members
+                ],
+            }
+        )
+
+        data["servers"].append(server_data)
+
+    read_states = await get_items(
+        filters={"user": current_user}, result_obj=ChannelReadState, current_user=current_user, size=None
+    )
+    data["read_states"] = [
+        {"channel": str(read_state.channel.pk), "last_read_at": read_state.last_read_at.isoformat()}
+        for read_state in read_states
+    ]
+
+    return data
