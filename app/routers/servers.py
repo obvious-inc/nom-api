0dc51426170ef512c9758f5b2301f9ea07b6bc51
prego <pedropregueiro@gmail.com>
2022-03-11 11:18:46 +0100

    new join server endpoint

--- app/routers/servers.py
+++ app/routers/servers.py
@@ -6,7 +6,7 @@ from fastapi import APIRouter, Body, Depends
 from app.dependencies import get_current_user
 from app.models.user import User
 from app.schemas.servers import ServerCreateSchema, ServerMemberSchema, ServerSchema
-from app.services.servers import create_server, get_server_members
+from app.services.servers import create_server, get_server_members, join_server
 
 router = APIRouter()
 
@@ -22,6 +22,12 @@ async def post_create_server(server: ServerCreateSchema = Body(...), current_use
     "/{server_id}/members",
     response_description="List server members",
     response_model=List[ServerMemberSchema],
+    status_code=http.HTTPStatus.CREATED,
 )
 async def get_list_server_members(server_id, current_user: User = Depends(get_current_user)):
     return await get_server_members(server_id, current_user=current_user)
+
+
+@router.post("/{server_id}/join", summary="Join server", response_model=ServerMemberSchema)
+async def post_join_server(server_id, current_user: User = Depends(get_current_user)):
+    return await join_server(server_id=server_id, current_user=current_user)
--- app/services/servers.py
+++ app/services/servers.py
@@ -8,19 +8,20 @@ from app.models.base import APIDocument
 from app.models.server import Server, ServerMember
 from app.models.user import User
 from app.schemas.servers import ServerCreateSchema
-from app.services.crud import create_item, get_item, get_items
+from app.services.crud import create_item, get_item, get_item_by_id, get_items
 
 
 async def create_server(server_model: ServerCreateSchema, current_user: User) -> Union[Server, APIDocument]:
     created_server = await create_item(server_model, result_obj=Server, current_user=current_user, user_field="owner")
 
     # add owner as server member
-    await join_server(created_server, current_user)
+    await join_server(server_id=str(created_server.pk), current_user=current_user)
 
     return created_server
 
 
-async def join_server(server: Union[Server, APIDocument], current_user: User) -> ServerMember:
+async def join_server(server_id: str, current_user: User) -> ServerMember:
+    server = await get_item_by_id(id_=server_id, result_obj=Server, current_user=current_user)
     member = ServerMember(server=server, user=current_user)
     await member.commit()
     return member
