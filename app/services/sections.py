5448fb377d7215e2d9d6bb1e3e4dcb2aa5546217
prego <pedropregueiro@gmail.com>
2022-04-11 11:41:50 +0100

    adding WS events for section changes

--- app/services/sections.py
+++ app/services/sections.py
@@ -4,11 +4,14 @@ from bson import ObjectId
 from fastapi import HTTPException
 from starlette import status
 
+from app.helpers.queue_utils import queue_bg_task
+from app.helpers.ws_events import WebSocketServerEvent
 from app.models.section import Section
 from app.models.server import Server
 from app.models.user import User
 from app.schemas.sections import SectionCreateSchema, SectionUpdateSchema
 from app.services.crud import create_item, delete_items, get_item_by_id, get_items, update_item
+from app.services.websockets import broadcast_server_event
 
 
 async def get_sections(server_id: str, current_user: User):
@@ -28,7 +31,17 @@ async def create_section(server_id: str, section_model: SectionCreateSchema, cur
 
     section_model.server = str(server.pk)
 
-    return await create_item(section_model, result_obj=Section, current_user=current_user, user_field=None)
+    section = await create_item(section_model, result_obj=Section, current_user=current_user, user_field=None)
+
+    await queue_bg_task(
+        broadcast_server_event,
+        server_id,
+        str(current_user.id),
+        WebSocketServerEvent.SERVER_SECTION_CREATE,
+        {"server": server_id, "section": section.to_dict()},
+    )
+
+    return section
 
 
 async def update_section(server_id: str, section_id: str, update_data: SectionUpdateSchema, current_user: User):
@@ -39,7 +52,17 @@ async def update_section(server_id: str, section_id: str, update_data: SectionUp
     data = update_data.dict(exclude_unset=True)
     section = await get_item_by_id(id_=section_id, result_obj=Section, current_user=current_user)
 
-    return await update_item(section, data=data, current_user=current_user)
+    updated_section = await update_item(section, data=data, current_user=current_user)
+
+    await queue_bg_task(
+        broadcast_server_event,
+        server_id,
+        str(current_user.id),
+        WebSocketServerEvent.SERVER_SECTION_UPDATE,
+        {"server": server_id, "section": updated_section.to_dict()},
+    )
+
+    return updated_section
 
 
 async def update_server_sections(server_id: str, sections: List[SectionCreateSchema], current_user: User):
@@ -55,5 +78,13 @@ async def update_server_sections(server_id: str, sections: List[SectionCreateSch
         section_model.server = str(server.pk)
         new_section = await create_item(section_model, result_obj=Section, current_user=current_user, user_field=None)
         final_sections.append(new_section)
+
+    await queue_bg_task(
+        broadcast_server_event,
+        server_id,
+        str(current_user.id),
+        WebSocketServerEvent.SERVER_SECTIONS_UPDATE,
+        {"server": server_id, "sections": final_sections},
+    )
 
     return final_sections
