ddca4e33eda2eb16e6e51e9835fefcdf4424f1c2
prego <pedropregueiro@gmail.com>
2022-04-12 13:22:27 +0100

    add a few limitations to params

--- app/tests/routers/test_dependencies.py
+++ app/tests/routers/test_dependencies.py
@@ -3,6 +3,7 @@ from bson import ObjectId
 from fastapi import FastAPI
 from httpx import AsyncClient
 
+from app.dependencies import common_parameters
 from app.models.channel import Channel
 
 
@@ -16,9 +17,43 @@ class TestDependenciesRouter:
             {"after": str(ObjectId()), "around": str(ObjectId())},
         ],
     )
-    async def test_dependencies_before(
+    async def test_common_params_before_or_after_or_around_only(
         self, app: FastAPI, authorized_client: AsyncClient, server_channel: Channel, query_params
     ):
-        print("query", query_params)
         response = await authorized_client.get(f"channels/{str(server_channel.pk)}/messages", params=query_params)
         assert response.status_code == 400
+
+    @pytest.mark.asyncio
+    @pytest.mark.parametrize("limit", [0, -20, 500, "hey"])
+    async def test_common_params_limit_within_limits_and_int(
+        self,
+        app: FastAPI,
+        authorized_client: AsyncClient,
+        server_channel: Channel,
+        limit: int,
+    ):
+        response = await authorized_client.get(f"channels/{str(server_channel.pk)}/messages?limit={limit}")
+        assert response.status_code == 422
+
+    @pytest.mark.asyncio
+    @pytest.mark.parametrize(
+        "sort, sort_by_field, sort_by_direction",
+        [
+            ("-created_at", "created_at", -1),
+            ("created_at", "created_at", 1),
+            ("-author", "author", -1),
+            ("author", "author", 1),
+        ],
+    )
+    async def test_common_params_sort_as_one_string(
+        self,
+        app: FastAPI,
+        authorized_client: AsyncClient,
+        server_channel: Channel,
+        sort,
+        sort_by_field,
+        sort_by_direction,
+    ):
+        params = await common_parameters(sort=sort)
+        assert params["sort_by_field"] == sort_by_field
+        assert params["sort_by_direction"] == sort_by_direction
