a4c474c03fa5f991e519786d9de75ab75dd4a650
prego <pedropregueiro@gmail.com>
2022-05-05 18:13:38 +0200

    breaking up stuff

--- app/helpers/permissions.py
+++ app/helpers/permissions.py
@@ -1,7 +1,7 @@
 import functools
 import logging
 from enum import Enum
-from typing import List
+from typing import List, Optional, Set
 
 from bson import ObjectId
 
@@ -58,113 +58,68 @@ async def has_permissions(needed_permissions: List[str], user_permissions: dict,
     return all([req_permission in overwritten_user_permissions for req_permission in needed_permissions])
 
 
-async def fetch_base_permissions(user: User, server: Server) -> List[str]:
-    permissions = []
-
-    if server.owner == user:
-        logger.debug("server owner has all permissions")
-        return ALL_PERMISSIONS
-
-    # TODO: add admin flag with specific permission overwrite
-
-    member = await get_item(filters={"server": server.pk, "user": user.pk}, result_obj=ServerMember, current_user=user)
-    if not member:
-        logger.warning("user (%s) doesn't belong to server (%s)", str(user.pk), str(server.pk))
-        return []
-
-    roles = [await role.fetch() for role in member.roles]
-    for role in roles:
-        logger.debug("extending permissions w/ role: %s (%s)", role.name, str(role.pk))
-        permissions.extend(role.permissions)
-
-    # remove duplicates
-    permissions = list(set(permissions))
-
-    return permissions
-
-
 async def fetch_permission_overwrites(channel_id: str = None, user: User = None):
     return []
 
 
-async def full_handle_permissions(func_args, func_kwargs, req_permissions):
-    permissions = [p.value for p in req_permissions]
-    logger.debug(f"required permissions: {permissions}")
+async def _fetch_channel_from_kwargs(kwargs: dict) -> Optional[str]:
+    channel_id = kwargs.get("channel_id")
 
-    # TODO: cache and reuse permission results
-
-    current_user = func_kwargs.get("current_user", None)  # type: User
-    if not current_user:
-        raise Exception(f"missing current_user from method. args: {func_args} | kwargs: {func_kwargs}")
-
-    channel_id = func_kwargs.get("channel_id")
-    server_id = None
-
-    channel_model = func_kwargs.get("channel_model")
-    if channel_model:
-        server_id = str(channel_model.server)
-
-    message_model = func_kwargs.get("message_model")
+    message_model = kwargs.get("message_model")
     if message_model and not channel_id:
         channel_id = str(message_model.channel)
 
-    if not channel_id and not server_id:
-        raise Exception(f"no channel and server found in kwargs: {func_kwargs}")
+    return channel_id
 
-    if server_id:
-        server = await get_item_by_id(id_=server_id, result_obj=Server, current_user=current_user)
-    elif channel_id:
-        channel = await get_item_by_id(id_=channel_id, result_obj=Channel, current_user=current_user)
-        if not channel.server:
-            logger.warning("ignoring complex permission checks for DMs")
-            return DEFAULT_DM_PERMISSIONS
-        server = await channel.server.fetch()
-    else:
-        raise Exception("missing channel_id or server_id from data")
 
-    # user_permissions = await fetch_base_permissions(user=current_user, server=server)
-    user_permissions = set()
+async def _fetch_server_from_kwargs(kwargs: dict) -> Optional[str]:
+    server_id = kwargs.get("server_id")
 
-    if server.owner == current_user:
-        logger.debug("server owner has all permissions")
-        return
+    channel_model = kwargs.get("channel_model")
+    if channel_model:
+        server_id = str(channel_model.server)
 
-    # TODO: add admin flag with specific permission overwrite
+    return server_id
+
+
+async def calc_permissions(current_user: User, channel_id: Optional[str], server: Server) -> Set[str]:
+    user_permissions: Set[str] = set()
 
     member = await get_item(
-        filters={"server": server.pk, "user": current_user.pk}, result_obj=ServerMember, current_user=current_user
+        filters={"server": server.pk, "user": current_user.pk},
+        result_obj=ServerMember,
+        current_user=current_user,
     )
     if not member:
         logger.warning("user (%s) doesn't belong to server (%s)", str(current_user.pk), str(server.pk))
-        raise APIPermissionError(needed_permissions=permissions, user_permissions=list(user_permissions))
-
-    if not channel_id and server:
-        # TODO: later, admins should be able to do some actions without channel (channels.create, etc.)
-        # TODO: refactor this code once everything is divided and conquered
-        logger.warning("without channel_id, using server-wide role permissions. ignoring any overwrites.")
-        server_wide_user_permissions = await fetch_base_permissions(user=current_user, server=server)
-        if not all([req_permission in server_wide_user_permissions for req_permission in permissions]):
-            raise APIPermissionError(needed_permissions=permissions, user_permissions=server_wide_user_permissions)
-        else:
-            return
-
-    channel = await get_item_by_id(id_=channel_id, result_obj=Channel, current_user=current_user)
-    section = await get_item(
-        filters={"server": server.pk, "channels": ObjectId(channel_id)}, result_obj=Section, current_user=current_user
-    )
-
-    section_overwrites = {}
-    if section:
-        section_overwrites = {
-            overwrite.role.pk: set(overwrite.permissions) for overwrite in section.permission_overwrites
-        }
-    channel_overwrites = {overwrite.role.pk: set(overwrite.permissions) for overwrite in channel.permission_overwrites}
-
-    logger.debug("section overwrites: %s", section_overwrites)
-    logger.debug("channel overwrites: %s", channel_overwrites)
+        return user_permissions
 
     roles = [await role.fetch() for role in member.roles]
     logger.debug("user has roles: %s", [(str(r.pk), r.name) for r in roles])
+
+    # fetch from cache
+    channel_overwrites = {}
+    section_overwrites = {}
+
+    if channel_id:
+        channel = await get_item_by_id(id_=channel_id, result_obj=Channel, current_user=current_user)
+        channel_overwrites = {
+            overwrite.role.pk: set(overwrite.permissions) for overwrite in channel.permission_overwrites
+        }
+
+        section = await get_item(
+            filters={"server": server.pk, "channels": ObjectId(channel_id)},
+            result_obj=Section,
+            current_user=current_user,
+        )
+
+        if section:
+            section_overwrites = {
+                overwrite.role.pk: set(overwrite.permissions) for overwrite in section.permission_overwrites
+            }
+
+    logger.debug("section overwrites: %s", section_overwrites)
+    logger.debug("channel overwrites: %s", channel_overwrites)
 
     for role in roles:
         logger.debug("fetching role permissions for: %s (%s)", role.name, str(role.pk))
@@ -182,19 +137,57 @@ async def full_handle_permissions(func_args, func_kwargs, req_permissions):
 
         user_permissions |= role_permissions
 
-    # remove duplicates
-    user_permissions = list(user_permissions)
-    logger.debug("user permissions: %s", user_permissions)
+    return user_permissions
 
-    if not all([req_permission in user_permissions for req_permission in permissions]):
-        raise APIPermissionError(needed_permissions=permissions, user_permissions=user_permissions)
+
+async def fetch_user_permissions(func_kwargs: dict, current_user: User) -> List[str]:
+    channel_id = await _fetch_channel_from_kwargs(func_kwargs)
+    server_id = await _fetch_server_from_kwargs(func_kwargs)
+
+    if not channel_id and not server_id:
+        logger.error("no channel and server found. kwargs: %s", func_kwargs)
+        raise Exception(f"no channel and server found in kwargs: {func_kwargs}")
+
+    # 3. fetch relevant data from DB (or cache?)
+    if server_id:
+        server = await get_item_by_id(id_=server_id, result_obj=Server, current_user=current_user)
+    elif channel_id:
+        channel = await get_item_by_id(id_=channel_id, result_obj=Channel, current_user=current_user)
+        if not channel.server:
+            return DEFAULT_DM_PERMISSIONS
+        server = await channel.server.fetch()
+    else:
+        raise Exception("missing channel_id or server_id from data")
+
+    if server.owner == current_user:
+        logger.debug("server owner has all permissions")
+        return ALL_PERMISSIONS
+
+    # TODO: add admin flag with specific permission overwrite
+
+    # 4. start calculating permissions
+    user_permissions = await calc_permissions(current_user=current_user, channel_id=channel_id, server=server)
+
+    return list(user_permissions)
 
 
 def needs(permissions):
     def decorator_needs(func):
         @functools.wraps(func)
         async def wrapper_needs(*args, **kwargs):
-            await full_handle_permissions(func_args=args, func_kwargs=kwargs, req_permissions=permissions)
+            str_permissions = [p.value for p in permissions]
+            logger.debug(f"required permissions: {str_permissions}")
+
+            current_user: User = kwargs.get("current_user", None)
+            if not current_user:
+                raise Exception(f"missing current_user from method. args: {args} | kwargs: {kwargs}")
+
+            user_permissions = await fetch_user_permissions(func_kwargs=kwargs, current_user=current_user)
+            logger.debug("user permissions: %s", user_permissions)
+
+            if not all([req_permission in user_permissions for req_permission in str_permissions]):
+                raise APIPermissionError(needed_permissions=str_permissions, user_permissions=user_permissions)
+
             value = await func(*args, **kwargs)
             return value
 
