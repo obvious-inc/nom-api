24ed8f886172a6dac906cbebd40c7af8f43144f8
prego <pedropregueiro@gmail.com>
2022-05-26 14:53:13 +0200

    Revert "use simplehash instead"
    This reverts commit d4677211ba0175593e32c8b1991c1bb0bb516433.

--- app/helpers/w3.py
+++ app/helpers/w3.py
@@ -70,7 +70,7 @@ async def _replace_with_cloudflare_gateway(token_image_url: str) -> str:
     return token_image_url
 
 
-async def get_nft(contract_address: str, token_id: str, provider: str = "simplehash") -> dict:
+async def get_nft(contract_address: str, token_id: str, provider: str = "alchemy") -> dict:
     if provider == "alchemy":
         return await get_alchemy_nft(contract_address, token_id)
     elif provider == "simplehash":
@@ -79,7 +79,7 @@ async def get_nft(contract_address: str, token_id: str, provider: str = "simpleh
         raise NotImplementedError("no other providers implemented")
 
 
-async def get_nft_image_url(nft, provider: str = "simplehash"):
+async def get_nft_image_url(nft, provider: str = "alchemy"):
     if provider == "alchemy":
         image_url = await get_alchemy_image_url(nft)
     elif provider == "simplehash":
