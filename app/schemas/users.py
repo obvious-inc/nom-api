8d014a58736356a8b6e5e8610fefe94c1febce89
prego <pedropregueiro@gmail.com>
2022-07-08 13:43:21 +0200

    fetch channel members and user channels

--- app/schemas/users.py
+++ app/schemas/users.py
@@ -71,3 +71,29 @@ class RoleCreateSchema(APIBaseCreateSchema):
     name: str
     server: Optional[str]
     permissions: List[str]
+
+
+class PublicUserSchema(APIBaseSchema):
+    display_name: Optional[str]
+    wallet_address: Optional[str]
+    pfp: Optional[dict]
+    status: Optional[str]
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "id": "61e17018c3ee162141baf5c8",
+                "display_name": "vitalik.eth",
+                "wallet_address": "0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045",
+                "pfp": {
+                    "cf_id": "5adcdc13-0a45-45cd-0707-31eab9997c00",
+                    "contract": "0x58f7e9810f5559dc759b731843212370363e433e",
+                    "token_id": "100",
+                    "token": {},
+                    "verified": True,
+                    "input": "https://opensea.io/assets/0x58f7e9810f5559dc759b731843212370363e433e/100",
+                    "input_image_url": "https://cloudflare-ipfs.com/ipfs/.../image.png",
+                },
+                "status": "online",
+            }
+        }
