7da0fc4fe034b24bc9d8863ba2b099c4e2db4401
prego <pedropregueiro@gmail.com>
2022-10-20 11:53:26 +0200

    remove unused web3 methods

--- app/helpers/w3.py
+++ app/helpers/w3.py
@@ -1,8 +1,6 @@
 import logging
 import re
-from functools import cache
 
-from ens import ENS
 from eth_account.messages import encode_defunct
 from eth_typing import ChecksumAddress
 from eth_utils import ValidationError
@@ -20,7 +18,7 @@ logger = logging.getLogger(__name__)
 
 
 def checksum_address(address: str) -> ChecksumAddress:
-    return Web3().toChecksumAddress(address)
+    return Web3.toChecksumAddress(address)
 
 
 def get_wallet_address_from_signed_message(message: str, signature: str) -> str:
@@ -30,28 +28,6 @@ def get_wallet_address_from_signed_message(message: str, signature: str) -> str:
     except ValidationError as e:
         raise ValueError(e)
     return address
-
-
-@cache
-def get_ens_primary_name_for_address(wallet_address: str) -> str:
-    settings = get_settings()
-    web3_client = Web3(Web3.WebsocketProvider(settings.web3_provider_url_ws))
-    wallet_address = checksum_address(wallet_address)
-    ens_name = ENS.fromWeb3(web3_client).name(wallet_address)
-    return ens_name
-
-
-async def get_wallet_short_name(address: str, check_ens: bool = True) -> str:
-    address = checksum_address(address)
-    short_address = f"{address[:5]}...{address[-3:]}"
-    if check_ens:
-        try:
-            ens_name = get_ens_primary_name_for_address(address)
-            short_address = ens_name or short_address
-        except Exception:
-            logger.exception("Problems fetching ENS primary domain. [address=%s]", address)
-
-    return short_address
 
 
 async def _replace_with_cloudflare_gateway(token_image_url: str) -> str:
