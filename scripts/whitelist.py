c9279d000d8700f8271dc7acae1272131ac4ce48
prego <pedropregueiro@gmail.com>
2022-12-07 18:40:34 +0100

    handle exceptions with unique wallets

--- scripts/whitelist.py
+++ scripts/whitelist.py
@@ -1,4 +1,5 @@
 import asyncio
+import logging
 import sys
 
 from asgi_lifespan import LifespanManager
@@ -6,12 +7,17 @@ from asgi_lifespan import LifespanManager
 from app.helpers.whitelist import whitelist_wallet
 from app.main import get_application
 
+logger = logging.getLogger(__name__)
+
 
 async def main():
     app = get_application()
     async with LifespanManager(app):
-        wallet_address = sys.argv[1]
-        await whitelist_wallet(wallet_address)
+        try:
+            wallet_address = sys.argv[1]
+            await whitelist_wallet(wallet_address)
+        except Exception as e:
+            logger.warning(f"problem whitelisting address {wallet_address}: {e}")
 
 
 if __name__ == "__main__":
