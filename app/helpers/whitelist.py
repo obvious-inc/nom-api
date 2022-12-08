b02f3a9f1ceed107edd6c3bd9b5b43edaa1ba585
prego <pedropregueiro@gmail.com>
2022-12-08 17:50:41 +0100

    remove unnecessary get_whitelisted

--- app/helpers/whitelist.py
+++ app/helpers/whitelist.py
@@ -1,23 +1,21 @@
 import logging
-from typing import List
 
+from app.helpers.w3 import checksum_address
 from app.models.user import WhitelistedWallet
-from app.services.crud import get_items
+from app.services.crud import get_item
 
 logger = logging.getLogger(__name__)
 
 
-async def get_whitelisted_wallets() -> List[WhitelistedWallet]:
-    return await get_items(filters={}, result_obj=WhitelistedWallet, limit=None)
-
-
 async def is_wallet_whitelisted(wallet_address: str) -> bool:
-    whitelisted_addresses = await get_whitelisted_wallets()
-    return wallet_address.lower() in [obj.wallet_address.lower() for obj in whitelisted_addresses]
+    wallet_address_checksum = checksum_address(wallet_address)
+    exists = await get_item(filters={"wallet_address": wallet_address_checksum}, result_obj=WhitelistedWallet)
+    return exists is not None
 
 
 async def whitelist_wallet(wallet_address: str) -> WhitelistedWallet:
-    db_object = WhitelistedWallet(wallet_address=wallet_address)
+    wallet_address_checksum = checksum_address(wallet_address)
+    db_object = WhitelistedWallet(wallet_address=wallet_address_checksum)
     await db_object.commit()
     logger.info("Object created. [object_type=%s, object_id=%s]", WhitelistedWallet.__name__, str(db_object.id))
     return db_object
