c7439c645e5fbba5a7c9d80dc31602f662691e63
prego <pedropregueiro@gmail.com>
2023-05-26 11:31:59 +0200

    pysha3 is deprecated 😅

--- app/helpers/crypto.py
+++ app/helpers/crypto.py
@@ -1,6 +1,6 @@
+from hashlib import sha3_256
 from typing import Tuple
 
-import sha3
 from cryptography.hazmat.primitives.asymmetric.ed25519 import Ed25519PrivateKey, Ed25519PublicKey
 
 
@@ -11,12 +11,12 @@ async def create_ed25519_keypair() -> Tuple[Ed25519PrivateKey, Ed25519PublicKey]
 
 
 async def sign_keccak_ed25519(data: bytes, private_key: Ed25519PrivateKey) -> bytes:
-    hash_val = sha3.keccak_256(data).digest()
+    hash_val = sha3_256(data).digest()
     signature = private_key.sign(hash_val)
     return signature
 
 
 async def verify_keccak_ed25519_signature(data: bytes, signature: bytes, signer: bytes):
-    hash_val = sha3.keccak_256(data).digest()
+    hash_val = sha3_256(data).digest()
     public_key = Ed25519PublicKey.from_public_bytes(signer)
     public_key.verify(signature, hash_val)
